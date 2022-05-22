95ec0a399a058ddc7eae3c30e42be3df6d498f96
Abhilash <abhilashyadav024@gmail.com>
2022-05-22 17:15:23 +0530

    color codes

--- 02-print.sh
+++ 02-print.sh
@@ -5,3 +5,10 @@
 echo hello
 echo abhilash
 echo how are you
+
+
+## to enable color code for your script
+## syntax: echo -e "\e[COLmMESSAGE\e[0m"
+
+##echo -e "\e[COLmMESSAGE\e[0m"
+echo -e"\e[COLmMESSAGE\e[0m"
